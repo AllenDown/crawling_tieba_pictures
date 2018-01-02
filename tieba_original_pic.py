5db392991ce83527f0d533dd866e35b0306f729f
Allen <707813012@qq.com>
2018-01-02 21:15:51 +0800

    add config.ini

--- tieba_original_pic.py
+++ tieba_original_pic.py
@@ -6,10 +6,27 @@
 from bs4 import BeautifulSoup
 import requests
 import time  # setting sleep time to avoid too many requests in short time
+import os
+import configparser
+
 
 urls = []  # saving the url that you want to use
-general_url = 'http://tieba.baidu.com/p/5349396535?pn={}'  # the general url
 num = 1
+config = configparser.ConfigParser()
+
+# get config info from config.ini
+with open('config.ini', 'r') as file:
+    config.read_file(file)
+    general_url = str(config.get('tieba', 'url'))
+    pages = config.getint('tieba', 'pages')
+    folder = config.get('tieba', 'folder')
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        print('create {} folder'.format(folder))
+    else:
+        print(folder+' has existed')
+    for i in range(1, pages+1):
+        urls.append(general_url.format(i))
 
 
 def get_pictures(url):
@@ -19,23 +36,15 @@ def get_pictures(url):
     images = Soup.find_all('img', 'BDE_Image')  # find the image tag
     for image in images:
         img = image.get('src')
-        original_img = img[0:30]+'pic/item/'+img[-44:]  # get real image path
-        with open('./NGZK_10/' + str(num) + '.jpg', 'wb') as f:
+        original_img = img[0:30]+'pic/item/'+img[-44:]  # get original image path
+        print(original_img)
+        with open('./'+folder+'/' + str(num) + '.jpg', 'wb') as f:
             f.write(requests.get(original_img).content)  # write the image into disk
             f.close()
-        print('getting {}th picture'.format(num))
+        print('get {}th picture'.format(num))
         num = num + 1
 
 
-# get pages what you want to deal with, and put url into a list
-def get_pages(nums):
-    for i in range(1, nums+1):
-        urls.append(general_url.format(i))
-
-
-get_pages(115)
-
-
 for u in urls:
-    get_pictures(u)
-    time.sleep(1)  # grab the picture!
+    get_pictures(u)  # grab the picture!
+    time.sleep(2)
